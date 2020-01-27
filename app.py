0506ad84fe9a56cc2b4fa94fb331f53dbde4da5f
akmatoff <azim.akmatov1@gmail.com>
2020-01-27 22:49:18 +0600

    tried to add a database

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = 'database.db'
+
+def get_db():
+  db = getattr(g, '_database', None)
+  if db is None:
+    db = g._database = sqlite3.connect(DATABASE)
+  return db
 
 app = Flask(__name__)
 app.debug = True
---
+++ table.sql
@@ -0,0 +1,6 @@
+UPDATE releases (
+    id int,
+    artist varchar(255),
+    title varchar(255),
+    genre varchar(255)
+);
