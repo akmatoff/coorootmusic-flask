57a0950352d15441cea62acafccb02c5572a63d1
akmatoff <azim.akmatov1@gmail.com>
2020-01-28 22:13:48 +0600

    Added releases

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def index():
   return render_template('home.html', releases = Releases)
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0')
+  app.run(host = 'localhost')
--- data.py
+++ data.py
@@ -44,7 +44,71 @@ def Releases():
       'album': 'Bottle Of Rum - Single',
       'cover': 'static/img/Releases/Cooroot - Bottle Of Rum.png',
       'pub-date': '2019-05-13'
-    }
+    },
+    {
+      'id': 6,
+      'artist': 'Cooroot',
+      'title': 'Feelings',
+      'genre': 'Chill',
+      'album': 'Feelings - Single',
+      'cover': 'static/img/Releases/Cooroot - Feelings.png',
+      'pub-date': '2019-05-15'
+    },
+    {
+      'id': 7,
+      'artist': 'Cooroot',
+      'title': 'Text Me',
+      'genre': 'Future Bass',
+      'album': 'Text Me - Single',
+      'cover': 'static/img/Releases/Cooroot - Text Me.png',
+      'pub-date': '2019-06-13'
+    },
+    {
+      'id': 8,
+      'artist': 'Cooroot',
+      'title': 'Butterflies',
+      'genre': 'Future Bass',
+      'album': 'Butterflies - Single',
+      'cover': 'static/img/Releases/Cooroot - Butterflies.png',
+      'pub-date': '2019-07-07'
+    },
+    {
+      'id': 9,
+      'artist': 'Cooroot',
+      'title': 'Lost',
+      'genre': 'Future Garage',
+      'album': 'Lost - Single',
+      'cover': 'static/img/Releases/Cooroot - Lost.png',
+      'pub-date': '2019-08-08'
+    },
+    {
+      'id': 10,
+      'artist': 'Cooroot',
+      'title': 'Purgatory',
+      'genre': 'Dubstep',
+      'album': 'Underground Unit: Sector 3',
+      'cover': 'static/img/Releases/Full Flex Audio - UUS3.png',
+      'pub-date': '2019-09-16'
+    },
+    {
+      'id': 11,
+      'artist': 'Cooroot',
+      'title': 'Rocket Science',
+      'genre': 'Dubstep',
+      'album': 'Rocket Science - Single',
+      'cover': 'static/img/Releases/Cooroot - Rocket Science.png',
+      'pub-date': '2019-11-02'
+    },
+    {
+      'id': 12,
+      'artist': 'Cooroot',
+      'title': 'Whatever',
+      'genre': 'Dubstep',
+      'album': 'Whatever - Single',
+      'cover': 'static/img/Releases/Cooroot - Whatever.png',
+      'pub-date': '2019-12-12'
+    },
+
   ]
 
   return releases
