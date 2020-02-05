7b95bd0ac6b76536241183beed75c689e36a0a92
akmatoff <azim.akmatov1@gmail.com>
2020-02-05 19:49:29 +0600

    got rid of unnecessary things

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 from flask import Flask, render_template, g
 from data import Releases
-import sqlite3
-
-DATABASE = 'database.db'
-
-def get_db():
-  db = getattr(g, '_database', None)
-  if db is None:
-    db = g._database = sqlite3.connect(DATABASE)
-  return db
 
 Releases = Releases()
 
