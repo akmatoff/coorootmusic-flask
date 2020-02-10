88c6e6c07720d370b341df12e4bb24e236eb0396
akmatoff <azim.akmatov1@gmail.com>
2020-02-10 21:50:08 +0600

    Created a database

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, redirect
+import sqlite3
 from data import Releases
 
 Releases = Releases()
+
 app = Flask(__name__)
 app.debug = True
 
@@ -11,7 +13,13 @@ def index():
 
 @app.route('/release/<string:id>/')
 def release(id):
-  return render_template('release.html', releases = Releases)  
+    with sqlite3.connect("coorootmusic.db") as con:
+      cur = con.cursor()
+      cur.execute("SELECT * FROM releases WHERE id = ?", [id])
+      release = cur.fetchone()
+
+    return render_template('release.html', release = release)  
+
 
 if __name__ == "__main__":
   app.run()
---
+++ createtable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE releases (
+    id integer primary key AUTOINCREMENT,
+    artist varchar(50),
+    title varchar(50),
+    genre varchar(50),
+    album varchar(50),
+    cover varchar(50),
+    pub_date varchar(50)
+);
